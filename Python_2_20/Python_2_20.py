7deb602b5bd17ee7bf453e888d4e5ce8eb049d44
zu-siwei <2150568643@qq.com>
2023-02-20 21:52:15 +0800

    python中学到的东西

---
+++ Python_2_20/Python_2_20.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+#for i in range(1,5):
+#    for j in range(1,5):
+#        for k in range(1,5):
+#            if( i != k ) and (i != j) and (j != k):
+#                print (i,j,k)
+
+#k = 1
+#sum = 0
+#for i in range(1,3):
+#    k = k * i
+#    sum = sum + k
+
+#print(sum)
+
+##运用三种模块的导入方法来求解30度的正弦函数值
+#import math #第一种方法
+#print(math.sin(math.radians(30)))
+
+#from math import *#第三种
+#print(sin(radians(30)))
+
+#from math import radians,sin
+#print(sin(radians(30)))
+
+from turtle import *
